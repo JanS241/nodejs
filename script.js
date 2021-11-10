1f65d536b75b0dbb66026f3a10091ef85721f45d
JanS241 <55439224+JanS241@users.noreply.github.com>
2021-11-10 21:01:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//new file
+console.log('Hello Winc Academy Students');
+let calc= 0;
+
+// next a large sum
+calc = 1230941 * 1823794;
+console.log("1230941 * 18233794 = ", calc);
+// a smaller sum
+calc = 637263 / 54;
+ console.log( "637263 /54  = " , calc);
+
+/* the next will not be seen on the screen
+calc = 637263 / 54;
+ console.log( "637263 /54  = " , calc);
+ because this is al seen as a comment */
+ 
